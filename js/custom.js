5a96c9b967b0fef90599420e11ceeec7663bc6f1
Fabian Merchan <fabian@merchan.co>
2024-06-19 22:52:13 -0600

    Update checkout.html with fake form data and fill button

--- js/custom.js
+++ js/custom.js
@@ -69,13 +69,37 @@
   };
   sitePlusMinus();
 
-  function disableLinks() {
+  (function disableLinks() {
     var links = document.querySelectorAll(".disabled");
     links.forEach(function (link) {
       link.addEventListener("click", function (e) {
         e.preventDefault();
       });
     });
+  })();
+
+  function fillForm() {
+    var form = document.getElementById("fake-form");
+    var inputs = form.querySelectorAll("input");
+
+    const fakeName = names[Math.floor(Math.random() * names.length)];
+    const fakeSurname = surnames[Math.floor(Math.random() * surnames.length)];
+    const fakeData = {
+      c_fname: fakeName,
+      c_lname: fakeSurname,
+      c_address: addresses[Math.floor(Math.random() * addresses.length)],
+      c_address_2: addresses2[Math.floor(Math.random() * addresses2.length)],
+      c_email_address: `${fakeName.toLowerCase()}.${fakeSurname.toLowerCase()}@clip.com`,
+      c_phone: phoneNumbers[Math.floor(Math.random() * phoneNumbers.length)],
+      c_state_country: states[Math.floor(Math.random() * states.length)],
+      c_postal_zip: postalCodes[Math.floor(Math.random() * postalCodes.length)],
+    };
+
+    inputs.forEach(function (input) {
+      input.value = fakeData[input.name];
+    });
   }
-  disableLinks();
+
+  var fillFormBtn = document.getElementById("fill-form");
+  fillFormBtn.addEventListener("click", fillForm);
 })();
